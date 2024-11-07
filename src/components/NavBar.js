0a5124bf3898da6182bed0b24db12dcfc7781ccf
Grace He <gracehe@10-17-60-33.dynapool.wireless.nyu.edu>
2024-11-07 16:12:58 -0500

    template for Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+// imports: 
+
+import React from "react"; // import react
+import './Navbar.css'; // import styling
+
+// Navbar function
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* Navbar content will go here */}
+        </nav>
+    )
+}
+
+// export
+
+export default Navbar;

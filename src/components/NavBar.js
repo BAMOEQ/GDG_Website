e1c2ba0069aa0e7428aa8a7462f597c324fe7891
NarezIn <simonni2004@outlook.com>
2024-11-10 15:57:24 -0500

    Modified NavBar and its style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,31 @@ import './Navbar.css'; // import styling
 const Navbar = () => {
     return (
         <nav className="navbar">
-            {/* Navbar content will go here */}
+            <div className="navbar-left">
+                <a href="/" className="logo"> {/* add homepage destination. */}
+                    <img src="/" alt="GDG Logo (delete after adding location)." classname = "logo-image" />
+                </a>
+            </div>
+            <div className="navbar-right">
+                <ul className="nav-links">
+                    <li>
+                        <a href="/">About Us</a>{/* add "About Us" page destination. */}
+                    </li>
+                    <li>
+                        <a href="/">Events</a>{/* add "Events" page destination. */}
+                    </li>
+                    <li>
+                        <a href="/">Projects</a>{/* add "Projects" page destination. */}
+                    </li>
+                    <li>
+                        <a href="/">Contact</a>{/* add "Contact" destination. */}
+                    </li>
+                    {/* Think of more NavBar pages to include.*/}
+                </ul>
+            </div>
         </nav>
     )
 }
 
 // export
-
 export default Navbar;

72b5b51078febeb565199cee1ee0f8b4f5bb387e
NarezIn <simonni2004@outlook.com>
2024-11-13 15:08:54 -0500

    Copy from main to NavBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Navbar from './components/NavBar';
 import HomePage from './components/HomePage';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -18,7 +18,6 @@ const App = () => {
                       {/*<Route path="/contact" element={<Contact />} />*/}
                   </Routes>
               </div>
-              <Footer />
           </div>
       </Router>
   );

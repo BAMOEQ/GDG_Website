7e6080c04b9382a44947d102da29b0f3d44ec0c7
NarezIn <simonni2004@outlook.com>
2024-11-20 17:43:24 -0500

    OurTeam

--- frontend/src/components/OurTeam/OurTeam.js
+++ frontend/src/components/OurTeam/OurTeam.js
@@ -1,16 +1,16 @@
 import React from "react"; // import react
 import './OurTeam.css'; // import styling
-//import back_card from '../../assets/back_card';
+import back_card from '../../assets/back_card';
 
 // ourteam function with dynamic name, title, and image
-// deal with the dynamic part later
+// Consider making name, title, and image later the dynamic part later
 const ourteam = (name, title, image) => {
     return (
         <nav className="ourteam">
             <div className="card">
                 {/*Should it be an individual page or inside about us */}
                 <a href="/ourteam" className="logo">
-                    <img src={image} alt={`${name}'s Card`} />
+                    <img src={back_card} alt={`${name}'s Card`} />
                         <h3>{name}</h3>
                     <p>{title}</p>
                 </a>

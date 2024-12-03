78bea7e09c1d29b178965257a73b0f8912537e97
Shivaika <shivaika3000@gmail.com>
2024-12-03 16:58:47 -0500

    added photo grid to disply event info/flyers

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,19 +1,37 @@
 import './Events.css';
 import React from 'react';
 import BannerImage from '../../assets/gdg_banner_red.jpg'
+import PhotoGrid from "./PhotoGrid";
 
 function Events() {
+  const eventFlyers = [
+    { src: '../../images/flyer1.jpg', alt: 'Flyer 1' },
+    { src: '../../images/flyer2.jpg', alt: 'Flyer 2' },
+    { src: '../../images/flyer3.jpg', alt: 'Flyer 3' },
+    { src: '../../images/flyer4.jpg', alt: 'Flyer 4' },
+    {},
+    { },
+    {  },
+    { },
+    {  },
+  ];
+
   return (
-    <div 
-    className="header" 
-    >
-      <img src={BannerImage} className="gdg-banner"></img>
-      <header className="headerContainer">
+    <div>
+      
+      <div className="header">
+       <img src={BannerImage} className="gdg-banner" alt="Banner" />
+       <header className="header-container">
+          <h1 className="gdg-title">Check out our events!</h1>
+       </header>
+     </div>
+
+
+
+      <div className="photo-grid-section">
         
-        <p>
-          Check out our events!
-        </p>
-      </header>
+        <PhotoGrid photos={eventFlyers} />
+      </div>
     </div>
   );
 }

a7d0e8a5767d35a09fd0a52d287a0ea5fc1d2d7a
Grace He <gh2313@nyu.edu>
2025-03-21 19:16:32 -0400

    updated our team to be dynamic

--- frontend/src/components/OurTeam/OurTeam.js
+++ frontend/src/components/OurTeam/OurTeam.js
@@ -1,22 +1,36 @@
-import React from "react"; // import react
+import React, {useEffect, useState} from "react"; // import react
 import './OurTeam.css'; // import styling
 import backcard from '../../assets/back_card.png';
 import headshot from '../../assets/head_shot.png';
+import axios from 'axios';
 
-// TODO: Consider making name, title, and image later the dynamic part later
 const OurTeam = () => {
+    const [officers, setOfficers] = useState([]);
+
+    useEffect (() => {
+        axios.get ('http://127.0.0.1:5000/api/officers')
+            .then(response => {
+                setOfficers(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching officers:", error);
+            });
+    }, []);
+
     return (
         <div className="ourteam-container">
             <div className="greet">
                 <h2>Meet the Team</h2>
             </div>
             <div className="card-container">
-                <div className="card">
-                    <img src={backcard} alt="Member's Card" className="backcard"/>
-                    <img src={headshot} alt="Headshot" className="headshot"/>
-                    <h3 className="member-name">Member's Name</h3>
-                    <p className="member-title">{"GDG Member"}</p>
-                </div>
+            {officers.map((officer) => (
+                    <div className="card" key = {officer.id}>
+                        <img src={backcard} alt="Member's Card" className="backcard"/>
+                        <img src={headshot} alt="Headshot" className="headshot"/>
+                        <h3 className="member-name">{officer.name}</h3>
+                        <p className="member-title">{officer.position}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )

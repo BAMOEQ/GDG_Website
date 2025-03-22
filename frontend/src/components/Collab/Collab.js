ceb1b9a70f6cf56eef337c33e5073604f13d293c
clammy424 <clammy424@gmail.com>
2025-03-21 22:14:25 -0400

    collab about

--- frontend/src/components/Collab/Collab.js
+++ frontend/src/components/Collab/Collab.js
@@ -7,9 +7,23 @@ function Collab(){
         <div>
             <div className="header">
                 <img src={BannerImage} className="gdg-banner" alt="Banner" />
-                <header className="header-container">
-                    <h1 className="gdg-title">Check out our projects!</h1>
-                </header>
+            </div>
+            
+            <div className="about-container">
+                <h1 style={{ fontSize: '30px' }}>
+                    <span style={{ color: '#555' }}>About</span> 
+                    <span style={{ color: '#faab00' }}> Col(l)ab</span>
+                </h1>
+
+            <div className='about-card'>
+                <p>
+                    Col(l)ab is an intensive 8 week program where students gain hands-on technical 
+                    skills and experience by working on real-world projects under mentorship
+                    of the E-Board and established professionals within the tech industry. 
+                    This program provides a space for participants to hone their technical skills, 
+                    network with our community, and gain further insight into pursuing a career in tech. 
+                </p>
+            </div>
             </div>
         </div>
     );

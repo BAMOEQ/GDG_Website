559df24414e2669421bf14e1a5d1a2b0436a263e
clammy424 <clammy424@gmail.com>
2025-03-21 23:25:06 -0400

    collab application button

--- frontend/src/components/Collab/Collab.js
+++ frontend/src/components/Collab/Collab.js
@@ -7,23 +7,34 @@ function Collab(){
         <div>
             <div className="header">
                 <img src={BannerImage} className="gdg-banner" alt="Banner" />
-            </div>
-            
-            <div className="about-container">
-                <h1 style={{ fontSize: '30px' }}>
-                    <span style={{ color: '#555' }}>About</span> 
-                    <span style={{ color: '#faab00' }}> Col(l)ab</span>
-                </h1>
 
-            <div className='about-card'>
-                <p>
-                    Col(l)ab is an intensive 8 week program where students gain hands-on technical 
-                    skills and experience by working on real-world projects under mentorship
-                    of the E-Board and established professionals within the tech industry. 
-                    This program provides a space for participants to hone their technical skills, 
-                    network with our community, and gain further insight into pursuing a career in tech. 
-                </p>
+                
+                <h1 style = {{color: '#333', marginBottom: '0px'}}>Fall 2025 Applications</h1>
+                <p style = {{color: '#555', font: '10px', marginTop: '0px'}}>Applications close on [date]</p>
+                <div className='button collab_application'>Collab Application</div>
+
+                <div className='about-card'>
+                    <h2 style={{ fontSize: '30px' }}>
+                        <span style={{ color: '#333' }}>About</span> 
+                        <span style={{ color: '#faab00' }}> Col(l)ab</span>
+                    </h2>
+                    <p style = {{color: "black", fontSize: '17px'}}> 
+                        Col(l)ab is an intensive 8 week program where students gain hands-on technical 
+                        skills and experience by working on real-world projects under mentorship
+                        of the E-Board and established professionals within the tech industry. 
+                        This program provides a space for participants to hone their technical skills, 
+                        network with our community, and gain further insight into pursuing a career in tech. 
+                        No experience is required, but passion and interest is!
+                    </p>
+                </div>
+
+                
+
+
             </div>
+
+            <div className="projects">
+                <h1 style={{color: '#333',fontSize: '30px'}}> Our Projects</h1>
             </div>
         </div>
     );

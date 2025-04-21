3b850b30981a02d0969bc0f3746acfd15e41cacf
clammy424 <122096602+clammy424@users.noreply.github.com>
2025-04-21 18:37:39 -0400

    collab application button (#24)
    * collab application button
    
    * linked the button
    
    * not sure if bold text in the button would look better lol
    
    * bolded button
    
    ---------
    
    Co-authored-by: Grace He <gh2313@nyu.edu>

--- frontend/src/components/Collab/Collab.js
+++ frontend/src/components/Collab/Collab.js
@@ -7,23 +7,34 @@ function Collab(){
         <div>
             <div className="header">
                 <img src={BannerImage} className="gdg-banner" alt="Banner" />
-            </div>
-            
-            <div className="about-container">
-                <h1 style={{ fontSize: '30px' }}>
-                    <span style={{ color: '#555' }}>About</span> 
-                    <span style={{ color: '#faab00' }}> Col(l)ab</span>
-                </h1>
 
-            <div className='about-card'>
-                <p>
-                    Col(l)ab is an intensive 8 week program where students gain hands-on technical 
-                    skills and experience by working on real-world projects under mentorship
-                    of the E-Board and established professionals within the tech industry. 
-                    This program provides a space for participants to hone their technical skills, 
-                    network with our community, and gain further insight into pursuing a career in tech. 
-                </p>
+                
+                <h1 style = {{color: '#333', marginBottom: '0px'}}>Fall 2025 Applications</h1>
+                <p style = {{color: '#555', font: '10px', marginTop: '5px'}}>Applications close [date]</p>
+                    <a href="https://forms.gle/Yz2Eh9UidniU8kFk7" class ="button collab_application" > <b>Apply to Collab</b></a>
+                
+
+                <div className='about-card'>
+
+                    <h2 style={{ fontSize: '30px' }}>
+                        <span style={{ color: '#333' }}>About</span> 
+                        <span style={{ color: '#faab00' }}> Col(l)ab</span>
+                    </h2>
+
+                    <p style = {{color: "black", fontSize: '17px'}}> 
+                        Col(l)ab is an intensive 8 week collaborative program where students gain hands-on technical 
+                        skills and experience by working on real-world projects under mentorship
+                        of our E-Board  and established professionals within the tech industry. 
+                        Col(l)ab opens up a space for participants to experiment and create alongside their peers. 
+                        Our program is an excellent opportunity for participants to hone in on their technical skills, 
+                        network with our community, and gain further insight into pursuing a career in tech. 
+                    </p>
+                </div>
+
             </div>
+
+            <div className="projects">
+                <h1 style={{color: '#333',fontSize: '30px'}}> Our Projects</h1>
             </div>
         </div>
     );

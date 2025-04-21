e4b312166d5823d7cd479d959e567c35c2bccc5a
Grace He <gh2313@nyu.edu>
2025-04-21 19:15:20 -0400

    project display, todo: edit card so it links to github, deployed link

--- frontend/src/components/Collab/Collab.js
+++ frontend/src/components/Collab/Collab.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Collab.css';
 import BannerImage from '../../assets/collab_banner.jpg'
+import axios from 'axios';
 
-function Collab(){
+const Collab = () =>{
+
+    const [groupedCollab, setGroupedCollab] = useState({});
+    
+    useEffect (() => {
+        axios.get ('http://127.0.0.1:5001/api/collab')
+            .then(response => {
+                const collab = response.data;
+                const grouped = collab.reduce((acc, collab) => {
+                    if (!acc[collab.year]) {
+                        acc[collab.year] = [];
+                    }
+                    acc[collab.year].push(collab);
+                    return acc;
+                }, {});
+                setGroupedCollab(grouped);
+            })
+            .catch(error => {
+                console.error("Error fetching projects:", error);
+            });
+    }, []);
+    
     return(
         <div>
             <div className="header">
@@ -11,7 +33,7 @@ function Collab(){
                 
                 <h1 style = {{color: '#333', marginBottom: '0px'}}>Fall 2025 Applications</h1>
                 <p style = {{color: '#555', font: '10px', marginTop: '5px'}}>Applications close [date]</p>
-                    <a href="https://forms.gle/Yz2Eh9UidniU8kFk7" class ="button collab_application" > <b>Apply to Collab</b></a>
+                    <a href="https://forms.gle/Yz2Eh9UidniU8kFk7" className ="button collab_application" > <b>Apply to Collab</b></a>
                 
 
                 <div className='about-card'>
@@ -30,12 +52,37 @@ function Collab(){
                         network with our community, and gain further insight into pursuing a career in tech. 
                     </p>
                 </div>
+                <div className="projects-container">
+                    <div className="project-header">
+                        <h3>Check Out Our Projects!</h3>
+                    </div>
 
+                    {Object.keys(groupedCollab).map(year => (
+                        <div key={year} className="project-section">
+                            <h3 className="project-name">{year}</h3>
+                            <div className="project-container">
+                                {groupedCollab[year].map((collab) => (
+                                    <div className="project-card" key={collab.id}>
+                                        {console.log(collab)}
+                                        <img
+                                            src={collab.image ? collab.image : "/images/default.png"}
+                                            alt={`${collab.name}'s Project`}
+                                            className="headshot"
+                                            onError={(e) => {
+                                                e.target.onerror = null;
+                                                e.target.src = "/images/default.png";
+                                            }}
+                                        />
+                                        <h3 className="collab-name">{collab.name}</h3>
+                                        <p className="collab-title">{collab.members}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
 
-            <div className="projects">
-                <h1 style={{color: '#333',fontSize: '30px'}}> Check Out Our Past Projects! </h1>
-            </div>
         </div>
     );
 }
